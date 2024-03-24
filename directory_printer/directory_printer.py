c526b78f92ef15d0cb0dcf5bbc07fa5be659c388
Thomas <thekileyenator@gmail.com>
2024-03-24 15:48:50 +0000

    Implement command to call the two functions

--- directory_printer/directory_printer.py
+++ directory_printer/directory_printer.py
@@ -1,8 +1,14 @@
+import os
 import click
+
+from directory_printer.print_directory_structure import print_directory_structure_output
+from directory_printer.build_directory_structure import build_directory_structure
 
 @click.command(help='Prints a the files and folders under current working directory.')
 def print_directories():
-    pass
+    current_working_directory = os.getcwd()
+    directory_structure = build_directory_structure(current_working_directory)
+    print_directory_structure_output(directory_structure)
 
 if __name__ == '__main__':
     print_directories()
