b8fabac68643230f3fc00720e68991b0ff01f9bf
Thomas <thekileyenator@gmail.com>
2024-03-24 14:45:03 +0000

    Set up basic command line infrastructure

---
+++ directory_printer/directory_printer.py
@@ -0,0 +1,8 @@
+import click
+
+@click.command(help='Prints a the files and folders under current working directory.')
+def print_directories():
+    pass
+
+if __name__ == '__main__':
+    print_directories()
