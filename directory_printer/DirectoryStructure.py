b752cd3d2354ff73ad2ad42619780af9c3822a29
Thomas <thekileyenator@gmail.com>
2024-03-24 15:16:17 +0000

    Add data structure for representing a directory tree

---
+++ directory_printer/DirectoryStructure.py
@@ -0,0 +1,8 @@
+class DirectoryStructure:
+    def __init__(self, dir_name: str, files: list[str], subdirectories:list['DirectoryStructure']) -> None:
+        # TODO: data validation on these inputs:
+        # - dir name is non empty valid folder name
+        # - subdirectories does not contain a cycle
+        self.dir_name = dir_name
+        self.files = files
+        self.subdirectories = subdirectories
